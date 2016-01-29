7c8cee47649f04d04c80af4c674d2019f12164f4
ehzShelter <emrulhasanzawad@outlook.com>
2016-01-29 01:12:04 -0800

    new file have been added

---
+++ appliance/install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo This bash script will download and install the vim from http://github.com/ehzShelter/dotMethod.
+read -p "Is that okay? [Yn] " confirm
+
+# Transform to lower case
+confirm=$(echo $confirm | tr [:upper:] [:lower:])
+
+if [[ -n $confirm && $confirm != "y" && $confirm != "yes" ]]; then
+  echo Please re-run this script in the home directories
+  exit 1
+fi
+
+echo
+echo Downloading and installing...
+mkdir -p ~/.dotMethod
+
+git clone --quiet https://github.com/ehzShelter/vimMethod ~/.dotMethod
+
+cd ~/.dotMethod/appliance/
+
+cp bashrc ~/.bashrc
+cp bash_profile ~/.bash_profile
+cp git-completion.bash ~/.git-completion.bash
+cp gvimrc ~/.gvimrc
+cp gitignore ~/.gitignore
+cp gitconfig ~/.gitconfig
+
+
+echo -e "\[\e[1;32m\]Everything succesfully installed.\[\e[0m\]"
+
