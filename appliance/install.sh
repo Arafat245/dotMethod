d70b3f536e80897aaf9d3d3c911633db8f305bd8
ehzShelter <emrulhasanzawad@outlook.com>
2016-03-05 05:35:21 -0500

    symbolic linked

--- appliance/install.sh
+++ appliance/install.sh
@@ -13,51 +13,62 @@ if [[ -n $confirm && $confirm != "y" && $confirm != "yes" ]]; then
   exit 1
 fi
 
-echo
+echo "hym"
 echo Downloading and installing...
-mkdir -p ~/.dotMethod
+
+
+if [ -d ~/.dotMethod ]
+then
+    echo "~/.dotMethod already exists"
+    echo "so removing......................"
+    rm -rf ~/.dotMethod
+fi
 
 git clone https://github.com/ehzShelter/dotMethod.git ~/.dotMethod
 echo Downloaded
 
 cd ~/.dotMethod/appliance/
 
-echo copying all dot file
+echo symbolic linking all dot file
 
-cp bashrc ~/.bashrc
-cp bash_profile ~/.bash_profile
-cp git-completion.bash ~/.git-completion.bash
-cp gvimrc ~/.gvimrc
-cp gitignore_global ~/.gitignore_global
-cp gitconfig ~/.gitconfig
-cp jscsrc  ~/.jscsrc
-cp jslintrc ~/.jslintrc
-cp jshintrc ~/.jshintrc
-cp profile ~/.profile
-cp clang-format ~/.clang-format
-cp tmux.conf ~/.tmux.conf
-cp inputrc ~/.inputrc
-cp npmrc ~/.npmrc
-cp json-compilation-databse ~/.json-compilation-databse
-cp tern-project ~/.tern-project
-
-mkdir -p ~/.npm-packages
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-echo "prefix I for installing tmux plugin"
-
-if [ -d "/usr/share/xfce4/terminal/" ]
-then
-sudo cp Smyck.theme /usr/share/xfce4/terminal/colorschemes/
-
-sudo chmod a+r /usr/share/xfce4/terminal/colorschemes/Smyck.theme
-else
-    echo " Sorry you are not using xfce"
-fi
-
+ln -fs .dotMethod/appliance/git-completion.bash ~/.git-completion.bash
+ln -fs .dotMethod/appliance/gvimrc ~/.gvimrc
+ln -fs .dotMethod/appliance/gitconfig ~/.gitconfig
+ln -fs .dotMethod/appliance/jscsrc  ~/.jscsrc
+ln -fs .dotMethod/appliance/jslintrc ~/.jslintrc
+ln -fs .dotMethod/appliance/jshintrc ~/.jshintrc
+ln -fs .dotMethod/appliance/profile ~/.profile
+ln -fs .dotMethod/appliance/clang-format ~/.clang-format
+ln -fs .dotMethod/appliance/tmux.conf ~/.tmux.conf
+ln -fs .dotMethod/appliance/inputrc ~/.inputrc
+ln -fs .dotMethod/appliance/npmrc ~/.npmrc
+ln -fs .dotMethod/appliance/json-compilation-databse ~/.json-compilation-databse
+ln -fs .dotMethod/appliance/tern-project ~/.tern-project
 ln -fs .dotMethod/appliance/gitignore_global  ~/.gitignore_global
 ln -fs .dotMethod/appliance/bash_profile ~/.bash_profile
 ln -fs .dotMethod/appliance/bashrc ~/.bashrc
 ln -fs .dotMethod/appliance/tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.npm-packages
+
+if [ -d ~/.tmux/plugins ]
+then
+    echo "Tmux exits"
+else
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+
+echo "prefix I for installing tmux plugin"
+
+if [ -d /usr/share/xfce4/terminal/ ]
+then
+    sudo cp Smyck.theme /usr/share/xfce4/terminal/colorschemes/
+
+    sudo chmod a+r /usr/share/xfce4/terminal/colorschemes/Smyck.theme
+else
+    echo " Sorry you are not using xfce"
+fi
+
 
 echo -e "\[\e[1;32m\]Everything succesfully installed.\[\e[0m\]"
